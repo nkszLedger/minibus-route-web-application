a1fb411d5281ca86f4a71331eacad69fe1458c9b
ZSingano <zsingano@csir.co.za>
2020-05-17 17:58:39 +0200

    fix(admin.role.index): Add style for role & permission management

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -1,96 +1,86 @@
-@extends('layouts.app')
-
+@extends('admin.home')
 
 @section('content')
-
-<div class="row">
-
-    <div class="col-lg-12 margin-tb">
-
-        <div class="pull-left">
-
-            <h2>Role Management</h2>
-
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="d-flex align-items-center">
+            <div class="mr-auto">
+                <h3 class="page-title">Roles Management</h3>
+                <div class="d-inline-block align-items-center">
+                    <nav>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="#"><i class="mdi mdi-home-outline"></i></a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Roles</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
         </div>
+    </div>
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="col-12">
+                <div class="box">
+                    <div class="box-header">
+                        <h4 class="box-title">Role Management Summary</h4>
+                        <div class="box-controls pull-right">
+                            @can('role-create')
+                                <a class="btn btn-success" href="{{ route('roles.create') }}"> Create New Role</a>
+                            @endcan
+                        </div>  
+                    </div>
+                    <div class="box-body">
+                        <div class="table-responsive">
+                            @if ($message = Session::get('success'))
+                                <div class="alert alert-success">
+                                    <p>{{ $message }}</p>
+                                </div>
+                            @endif
+                            <table id="complex_header" class="table table-striped table-bordered display" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Name</th>
+                                        <th width="280px">Action</th>
+                                        {{-- <th></th> --}}
+                                    </tr>
+                                </thead>
 
-        <div class="pull-right">
+                                <tbody>
+                                    @foreach ($roles as $key => $role)
+                                    <tr>
+                                        <td>{{ ++$i }}</td>
 
-        @can('role-create')
+                                        <td>{{ $role->name }}</td>
+                                        <td>
+                                            <a class="btn btn-info" href="{{ route('roles.show',$role->id) }}">Show</a>
 
-            <a class="btn btn-success" href="{{ route('roles.create') }}"> Create New Role</a>
+                                            @can('role-edit')
+                                                <a class="btn btn-primary" href="{{ route('roles.edit',$role->id) }}">Edit</a>
+                                            @endcan
 
-            @endcan
+                                            @can('role-delete')
+                                                {!! Form::open(['method' => 'DELETE','route' => ['roles.destroy', $role->id],'style'=>'display:inline']) !!}
 
+                                                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+
+                                                {!! Form::close() !!}
+                                            @endcan
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                   
+                                </tbody>
+                            </table>
+                            {!! $roles->render() !!}
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
-    </div>
-
-</div>
-
-
-@if ($message = Session::get('success'))
-
-    <div class="alert alert-success">
-
-        <p>{{ $message }}</p>
-
-    </div>
-
-@endif
-
-
-<table class="table table-bordered">
-
-  <tr>
-
-     <th>No</th>
-
-     <th>Name</th>
-
-     <th width="280px">Action</th>
-
-  </tr>
-
-    @foreach ($roles as $key => $role)
-
-    <tr>
-
-        <td>{{ ++$i }}</td>
-
-        <td>{{ $role->name }}</td>
-
-        <td>
-
-            <a class="btn btn-info" href="{{ route('roles.show',$role->id) }}">Show</a>
-
-            @can('role-edit')
-
-                <a class="btn btn-primary" href="{{ route('roles.edit',$role->id) }}">Edit</a>
-
-            @endcan
-
-            @can('role-delete')
-
-                {!! Form::open(['method' => 'DELETE','route' => ['roles.destroy', $role->id],'style'=>'display:inline']) !!}
-
-                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-
-                {!! Form::close() !!}
-
-            @endcan
-
-        </td>
-
-    </tr>
-
-    @endforeach
-
-</table>
-
-
-{!! $roles->render() !!}
-
-
-<p class="text-center text-primary"><small>Tutorial by ItSolutionStuff.com</small></p>
-
+        <!-- /.row -->
+    </section>
+<!-- /.content -->
 @endsection
