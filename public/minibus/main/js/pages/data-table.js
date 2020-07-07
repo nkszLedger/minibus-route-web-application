5b49a2c379786d4b58e0440ec06cc34776c9edec
ZSingano <zsingano@csir.co.za>
2020-07-07 05:39:03 +0200

    fix(data-table.js): Remove ninth column on table upon exporting document

--- public/minibus/main/js/pages/data-table.js
+++ public/minibus/main/js/pages/data-table.js
@@ -22,20 +22,20 @@ $(function () {
             { 
                 extend: 'csv', 
                 exportOptions: {
-                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8]
                 },
             },
             {
                 extend: 'excel',
                 exportOptions: {
-                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8]
                 },
             },
             {
                 extend: 'pdf', 
                 orientation: 'landscape',
                 exportOptions: {
-                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8]
                 },
 
             },
@@ -43,7 +43,7 @@ $(function () {
                 extend: 'print',
                 orientation: 'landscape',
                 exportOptions: {
-                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8]
                 },
             }
             //'copy', 'csv', 'excel', 'pdf', 'print'
